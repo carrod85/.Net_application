44d7948173b9aea5281c1127b9b0797e37c8f6c3
Carlos Rodriguez <carrod@ttu.ee>
2022-09-20 16:12:33 +0300

    reupload files

---
+++ crypto/consoleApp01/Program.cs
@@ -0,0 +1,46 @@
+﻿using ConsoleApp01;
+
+Console.WriteLine("Hello, Cesar!");
+
+string defaultAlphabet = "ABCDEFGHIJKLMNOPQRSTUVXYZ!?.";
+
+/*get the alphabet with input validation*/
+
+
+
+
+// TODO: write a input function that would correctly validate user input and return valid numeric value at the end
+// look into int.TryParse()
+/*
+var checkShift = Helper.GetUserShift(alphabet.Length);
+Console.WriteLine($"Shift: {checkShift}");
+if (checkShift == 0)
+{   Console.WriteLine("program finished");
+    return 0;
+}
+*/
+//int checkShift;
+// string checkText;
+//string encryption;
+
+try
+{ 
+    var alphabet = Helper.GetUserAlphabet(defaultAlphabet);
+    Console.WriteLine($"Alphabet: {alphabet}");
+    int checkShift = Helper.GetUserShift(alphabet.Length);
+    Console.WriteLine($"Shift: {checkShift}");
+    string checkText = Helper.GetPlainText(alphabet);
+    Console.WriteLine($"Text: {checkText}");
+    string encryption = Helper.GetEncryption(alphabet, checkShift, checkText);
+    Console.WriteLine($"Encryption: {encryption}");
+}
+catch(Exception e)
+{
+    Console.WriteLine(e.Message);
+}
+
+
+/*
+var encryption = Helper.GetEncryption(alphabet, checkShift, checkText);
+Console.WriteLine($"Encryption: {encryption}");
+*/
---
+++ crypto/consoleApp01/ShiftHelp.cs
@@ -0,0 +1,45 @@
+namespace ConsoleApp01;
+
+public static class ShiftHelp
+{
+    public static int GetUserShift(int lenghtAlpha)
+    {
+        bool isValidInput = false;
+        int result = 0;
+        
+        while(!isValidInput)
+        {
+            result = 0;
+            Console.Write("Cesar shift amount or type 'exit' to finish: ");
+            string? userInput = Console.ReadLine();
+
+
+            if (userInput == "exit" )
+            {
+                Console.WriteLine("Finishing program");
+                break;
+            }
+            else if (string.IsNullOrWhiteSpace(userInput)) // is null or empty or white space
+            {
+                Console.WriteLine("incorrect input");
+
+            }
+            else if (int.TryParse(userInput, out result))
+            {
+                if (Math.Abs(result) >= lenghtAlpha || result == 0)
+                {
+                    Console.WriteLine($"number different than 0 and less than |{lenghtAlpha}| alphabet needed!!");
+                    continue;
+
+                }
+
+                isValidInput = true;
+
+            }
+
+
+        }
+
+        return result;
+    }
+}
